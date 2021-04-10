5573033cf8e0a44ad1d8e404dd976fb7d2b0934b
AEBIEE <abdallahebnmg@gnail.com>
2021-04-10 16:01:27 +0200

    first own practice project on gitHib

---
+++ script.js
@@ -0,0 +1,13 @@
+const panels = document.querySelectorAll('.panel');
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeAcriveClasses();
+        panel.classList.add('active');
+    })
+})
+
+function removeAcriveClasses() {
+    panels.forEach( panel => {
+        panel.classList.remove('active')
+    })
+}
